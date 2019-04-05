4a8178612b79c81ba8669f36b6f888a447bee9d4
MakaoPoo <miyamasa0421@gmail.com>
2019-04-05 15:09:53 +0900

    音を追加 細かい挙動を変更

---
+++ js/WebAudioAPI.js
@@ -0,0 +1,53 @@
+let audioCtx = null;
+
+function initWebAPI() {
+  try {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    audioCtx = new AudioContext();
+  }
+  catch(e) {
+    audioCtx = null;
+    alert("このブラウザではWeb Audio APIがサポートされていません。");
+  }
+}
+
+function loadSound(name, loaded){
+  if (!audioCtx) { return; }
+  const request = new XMLHttpRequest();
+  const url = "/resource/" + name;
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+  request.onload = function() {
+    audioCtx.decodeAudioData(
+      request.response,
+      function(bufferData) {
+        loaded(bufferData);
+      },
+      function() {
+        alert("音声ファイルの読み込みに失敗しました。");
+      });
+  }
+  request.send();
+}
+
+let prevSource = null;
+
+function playSound(buffer){
+  if(buffer == null) {
+    return;
+  }
+  const source = audioCtx.createBufferSource();
+  source.buffer = buffer;
+  source.connect(audioCtx.destination);
+  if(prevSource != null) {
+    prevSource.stop();
+  }
+  source.start(0);
+  prevSource = source;
+}
+
+function playSoundSilent() {
+  const emptySource = audioCtx.createBufferSource();
+  emptySource.start();
+  emptySource.stop();
+}
--- js/main.js
+++ js/main.js
@@ -1,114 +1,144 @@
-const config = {
-apiKey: "AIzaSyBJ9spM0Gq02Jrq55TutDHVSDXKVW_mlfc",
-authDomain: "asamadesoreseikai.firebaseapp.com",
-databaseURL: "https://asamadesoreseikai.firebaseio.com",
-storageBucket: "asamadesoreseikai.appspot.com",
-};
-firebase.initializeApp(config);
+$(function() {
+  const config = {
+    apiKey: "AIzaSyBJ9spM0Gq02Jrq55TutDHVSDXKVW_mlfc",
+    authDomain: "asamadesoreseikai.firebaseapp.com",
+    databaseURL: "https://asamadesoreseikai.firebaseio.com",
+    storageBucket: "asamadesoreseikai.appspot.com",
+  };
+  firebase.initializeApp(config);
 
-const db = firebase.database();
+  const db = firebase.database();
 
-const dbQuestion = db.ref('/question');
-dbQuestion.on("value", function(snapshot) {
-  const questionText = snapshot.val();
-  const $odaiText = $('#odaitext');
-  $odaiText.val(questionText).trigger('keyup');
-});
-
-for(let id = 0; id < 8; id++) {
-  const dbAnswer = db.ref('/user_list/user' + id + '/answer');
-  dbAnswer.on("value", function(snapshot) {
-    const answerText = snapshot.val();
-    const $user = $('.user[data-id="' + id + '"]');
-    const $panelText = $user.find('.panel > p');
-    $panelText.text(answerText);
+  const dbQuestion = db.ref('/question');
+  dbQuestion.on("value", function(snapshot) {
+    const questionText = snapshot.val();
+    const $odaiText = $('#odaitext');
+    $odaiText.val(questionText).trigger('keyup');
+    playSound(dededen);
   });
 
-  const dbOpen = db.ref('/user_list/user' + id + '/is_open');
-  dbOpen.on("value", function(snapshot) {
-    const is_open = snapshot.val();
-    const $user = $('.user[data-id="' + id + '"]');
-    const $panel = $user.find('.panel');
-    if(is_open) {
-      $panel.addClass('open');
-    } else {
-      $panel.removeClass('open');
+  for(let id = 0; id < 8; id++) {
+    const dbAnswer = db.ref('/user_list/user' + id + '/answer');
+    dbAnswer.on("value", function(snapshot) {
+      const answerText = snapshot.val();
+      const $user = $('.user[data-id="' + id + '"]');
+      const $panelText = $user.find('.panel > p');
+      $panelText.text(answerText);
+    });
+
+    const dbOpen = db.ref('/user_list/user' + id + '/is_open');
+    dbOpen.on("value", function(snapshot) {
+      const is_open = snapshot.val();
+      const $user = $('.user[data-id="' + id + '"]');
+      const $panel = $user.find('.panel');
+      if(is_open) {
+        if(!$panel.hasClass('open')) {
+          playSound(deden);
+        }
+        $panel.addClass('open');
+      } else {
+        $panel.removeClass('open');
+      }
+    });
+
+    const dbName = db.ref('/user_list/user' + id + '/name');
+    dbName.on("value", function(snapshot) {
+      const answerText = snapshot.val();
+      const $user = $('.user[data-id="' + id + '"]');
+      const $nameText = $user.find('.name > p');
+      $nameText.text(answerText);
+    });
+  }
+
+  $('.user').on('click', function() {
+    if($(this).hasClass('selected')) {
+      return;
+    }
+    const name = window.prompt("ユーザー名", "");
+
+    if(name != "" && name != null) {
+      const id = $(this).data('id');
+
+      $('.user').removeClass('selected');
+      $(this).addClass('selected');
+
+      const dbName = db.ref('/user_list/user' + id + '/name');
+      const dbAnswer = db.ref('/user_list/user' + id + '/answer');
+      const dbOpen = db.ref('/user_list/user' + id + '/is_open');
+
+      dbName.set(name);
+      dbAnswer.set("");
+      dbOpen.set(false);
+    } else if(name == "") {
+      window.alert("ユーザー名が入力されていません");
     }
   });
 
-  const dbName = db.ref('/user_list/user' + id + '/name');
-  dbName.on("value", function(snapshot) {
-    const answerText = snapshot.val();
-    const $user = $('.user[data-id="' + id + '"]');
-    const $nameText = $user.find('.name > p');
-    $nameText.text(answerText);
+  $('.title').on('click', function() {
+    const odaitext = $('#odaitext').val();
+
+    const dbQuestion = db.ref('/question');
+    dbQuestion.set(odaitext);
   });
-}
 
-$('.user').on('click', function() {
-	const name = window.prompt("ユーザー名", "");
+  $('#open_btn').on('click', function() {
+    const $selectUser = $('.user.selected');
+    if($selectUser.length != 1) {
+      window.alert('フリップを選択してください');
+      return;
+    }
+    const answer = $('#answer_input').val();
+    if(answer == "" || answer == null) {
+      window.alert('回答を入力してください');
+      return;
+    }
 
-  if(name != "" && name != null) {
-    const id = $(this).data('id');
+    const id = $selectUser.data('id');
 
-    $('.user').removeClass('selected');
-    $(this).addClass('selected');
+    const dbAnswer = db.ref('/user_list/user' + id + '/answer');
+    const dbOpen = db.ref('/user_list/user' + id + '/is_open');
+    dbAnswer.set(answer);
+    dbOpen.set(true);
+  });
 
-    const dbAnswer = db.ref('/user_list/user' + id + '/name');
-    dbAnswer.set(name);
-  } else if(name == "") {
-  	window.alert("ユーザー名が入力されていません");
-  }
+  $('#reset_btn').on('click', function() {
+    const $selectUser = $('.user.selected');
+    if($selectUser.length != 1) {
+      window.alert('フリップを選択してください');
+      return;
+    }
+    const id = $selectUser.data('id');
+
+    const dbAnswer = db.ref('/user_list/user' + id + '/answer');
+    const dbOpen = db.ref('/user_list/user' + id + '/is_open');
+    dbAnswer.set("");
+    dbOpen.set(false);
+    $('#answer_input').val("");
+  });
+
+  $(window).on('resize', function() {
+    $('#odaitext').trigger('keyup');
+  });
+
+  $('#odaitext').keyup(function() {
+    $(this).css('height', 'auto');
+    const height = $(this)[0].scrollHeight;
+    $(this).css('height', height);
+  });
 });
 
-$('.title').on('click', function() {
-  const odaitext = $('#odaitext').val();
+let deden;
+let dededen;
+$(window).on('mousedown touchdown', function() {
+  initWebAPI();
 
-  const dbQuestion = db.ref('/question');
-  dbQuestion.set(odaitext);
-});
+  loadSound("deden.wav", function(bufferData) {
+    deden = bufferData;
+  });
 
-$('#open_btn').on('click', function() {
-  const $selectUser = $('.user.selected');
-  if($selectUser.length != 1) {
-    window.alert('フリップを選択してください');
-    return;
-  }
-  const answer = $('#answer_input').val();
-  if(answer == "" || answer == null) {
-    window.alert('回答を入力してください');
-    return;
-  }
+  loadSound("dededen.wav", function(bufferData) {
+    dededen = bufferData;
+  });
 
-  const id = $selectUser.data('id');
-
-  const dbAnswer = db.ref('/user_list/user' + id + '/answer');
-  const dbOpen = db.ref('/user_list/user' + id + '/is_open');
-  dbAnswer.set(answer);
-  dbOpen.set(true);
-});
-
-$('#reset_btn').on('click', function() {
-  const $selectUser = $('.user.selected');
-  if($selectUser.length != 1) {
-    window.alert('フリップを選択してください');
-    return;
-  }
-  const id = $selectUser.data('id');
-
-  const dbAnswer = db.ref('/user_list/user' + id + '/answer');
-  const dbOpen = db.ref('/user_list/user' + id + '/is_open');
-  dbAnswer.set("");
-  dbOpen.set(false);
-   $('#answer_input').val("");
-});
-
-$(window).on('resize', function() {
-  $('#odaitext').trigger('keyup');
-});
-
-$('#odaitext').keyup(function() {
-  $(this).css('height', 'auto');
-  const height = $(this)[0].scrollHeight;
-  $(this).css('height', height);
+  $(window).off('mousedown touchdown');
 });
